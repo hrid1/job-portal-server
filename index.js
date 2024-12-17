e5233cd7a30edbdd93e5fc5524d725c4f8e405d4
hrid1 <hridoycse1@gmail.com>
2024-12-17 23:53:45 +0600

    Token Added to the Cookie

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
@@ -31,13 +33,18 @@ async function run() {
     const applicationCollection = client
       .db("JobPortal")
       .collection("applications");
-    
-      // Auth related APIs
-      app.post('/jwt', async (req, res) => {
-        const user = req.body;
-        const token = jwt.sign(user, "secret", {expiresIn: '1h'} )
-        res.send(token);
-      })
+
+    // Auth related APIs
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+      });
+      res.send({ success: true });
+    });
 
     // Jobs APIs
     app.get("/jobs", async (req, res) => {
